22106295a8ab572c1a54d02a1009ec5059f10a1a
andriomdac <andriomdac@gmail.com>
2025-08-04 17:58:42 -0300

    general correction

--- gymanager-api/app/urls.py
+++ gymanager-api/app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/gyms/', include('gyms.urls')),
     path('api/students/', include('students.urls')),
-    path('api/students/<str:student_id/payments/', include('payments.urls')),
+    path('api/students/<str:student_id>/payments/', include('payments.urls')),
     path('api/payment_methods/', include('payment_methods.urls')),
 #    path('api/payment_packages/', include('payment_packages.urls')),
 ]
--- gymanager-api/payment_packages/models.py
+++ gymanager-api/payment_packages/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from uuid import uuid4
 
 class PaymentPackage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=10)
 
     def __str__(self):
