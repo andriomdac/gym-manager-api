c7a9866c25e4ffdb5421c9c2b9d266e9f7b809f9
andriomdac <andriomdac@gmail.com>
2025-08-10 12:44:34 -0300

    add duration_days field to model

--- gymanager-api/payment_packages/models.py
+++ gymanager-api/payment_packages/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from uuid import uuid4
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class PaymentPackage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=10)
+    duration_days = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(372)])
 
     def __str__(self):
         return self.name
