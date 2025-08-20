40e6ef1fd4702a03bbaea97bf26235552038b06a
andriomdac <andriomdac@gmail.com>
2025-08-19 22:36:29 -0300

    creating nasting resourse in url

--- gymanager-api/app/urls.py
+++ gymanager-api/app/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/gyms/', include('gyms.urls')),
     path('api/gyms/<str:gym_id>/students/', include('students.urls')),
-    path('api/students/<str:student_id>/payments/', include('payments.urls')),
-    path('api/cash_registers/', include('cash_registers.urls')),
+    path('api/gyms/<str:gym_id>/students/<str:student_id>/payments/', include('payments.urls')),
+    path('api/gyms/<str:gym_id>/cash-registers/', include('cash_registers.urls')),
 
-    path('api/payment_methods/', include('payment_methods.urls')),
-    path('api/payment_packages/', include('payment_packages.urls')),
+    path('api/payment-methods/', include('payment_methods.urls')),
+    path('api/payment-packages/', include('payment_packages.urls')),
 ]
