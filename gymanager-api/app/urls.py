5b60965b718ba9ef66aa69e4dfca48d01e311bcb
andriomdac <andriomdac@gmail.com>
2025-08-23 21:11:18 -0300

    minor mods

--- gymanager-api/app/urls.py
+++ gymanager-api/app/urls.py
@@ -5,8 +5,10 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/gyms/', include('gyms.urls')),
-    path('api/gyms/<str:gym_id>/students/', include('students.urls')),
+
+    path('api/gyms/<str:gym_id>/students/', include('students.urls')), 
     path('api/gyms/<str:gym_id>/students/<str:student_id>/payments/', include('payments.urls')),
+
     path('api/gyms/<str:gym_id>/cash-registers/', include('cash_registers.urls')),
 
     path('api/payment-methods/', include('payment_methods.urls')),
