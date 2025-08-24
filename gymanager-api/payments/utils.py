cfe35595ff6e7cb10638409315fab521d61f55e3
andriomdac <andriomdac@gmail.com>
2025-08-23 21:00:01 -0300

    check if payment_package_id is available

--- gymanager-api/payments/utils.py
+++ gymanager-api/payments/utils.py
@@ -1,13 +1,11 @@
-# Standard library imports
 from datetime import datetime, timedelta
-
-# Third-party imports
 from rest_framework.generics import get_object_or_404
-
-# Local application imports
 from payment_packages.models import PaymentPackage
 
 
 def get_next_payment_date(payment_date: datetime, payment_package_id: str) -> datetime:
+    if not payment_package_id:
+        raise CustomValidatorException("'payment_package' field missing")
+
     payment_package = get_object_or_404(PaymentPackage, id=payment_package_id)
     return payment_date + timedelta(days=payment_package.duration_days)
