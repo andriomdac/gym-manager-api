c57614910b96233528cf0c559e74738751e18113
andriomdac <andriomdac@gmail.com>
2025-09-12 20:31:03 -0300

    implementing jwt in apps

--- gymanager-api/cash_registers/models.py
+++ gymanager-api/cash_registers/models.py
@@ -3,10 +3,11 @@ from uuid import uuid4
 from django.core.validators import MinValueValidator
 from gyms.models import Gym
 
+
 class CashRegister(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True, unique=True)
     uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
-    gym = models.ForeignKey(to=Gym, on_delete=models.PROTECT, related_name="cash_registers")
+    gym = models.ForeignKey(to=Gym, on_delete=models.PROTECT, related_name="cash_registers", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     register_date = models.DateField(unique=True)
     is_opened = models.BooleanField(default=True)
