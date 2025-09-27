d74f4a49d29dbaec671ad1435b3d02e8cfa2c95d
andriomdac <andriomdac@gmail.com>
2025-09-27 16:43:25 -0300

    script for transfering data development

--- gymanager-api/transferdata.py
+++ gymanager-api/transferdata.py
@@ -1,7 +1,6 @@
 import json
 from icecream import ic
 
-
 with open("data.json", "r") as file:
     data = json.load(file)
 
@@ -17,29 +16,41 @@ for i in data:
                 "name": i["fields"]["name"],
                 "phone": i["fields"]["phone"],
                 "reference": i["fields"]["reference"],
-                "payments": []
-                }
-            )
+                "payments": [],
+            }
+        )
 
 
 for i in data:
     if i["model"] == "payments.payment":
         payment_student_pk = i["fields"]["student"]
         payment_fields = i["fields"]
-        
+
         for student in students:
             if student["pk"] == payment_student_pk:
                 payment = {
+                    "pk": i["pk"],
                     "payment_date": payment_fields["payment_date"],
                     "next_payment_date": payment_fields["next_payment_date"],
                     "payment_package": payment_fields["payment_package"],
                     "observations": payment_fields["observations"],
                     "user_signature": payment_fields["user_signature"],
                     "created_at": payment_fields["created_at"],
-                    "values": []
+                    "values": [],
                 }
                 student["payments"].append(payment)
 
+for i in data:
+    if i["model"] == "payments.paymentvalue":
+        paymentvalue_payment_id = i["fields"]["payment"]
+        for student in students:
+            for payment in student["payments"]:
+                if payment["pk"] == paymentvalue_payment_id:
+                    payment["values"].append(i["fields"])
 
-print(json.dumps(students, indent=4))
-                
+                if len(payment["values"]) > 1:
+                    print(json.dumps(payment, indent=4))
+
+
+with open("cleaned_data.json", "w") as cleaned_data:
+    json.dump(students, cleaned_data, indent=4, ensure_ascii=False)
